9ecf9ca5e04ae7023dccc120fcd0b04f8991a40e
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-05 23:09:59 +0200

    Lesson 20

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require("fs");
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require ('./modules/replaceTemplate');
 
 
@@ -50,6 +51,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map( el => slugify(el.productName, {lower: true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     
