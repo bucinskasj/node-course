ea004d3bba69465b470103c790360b7268986edd
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-07 09:31:44 +0200

    Wrapping up section 5

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro("`${__dirname}/dog.txt`").then((data) => {
   console.log(`Breed: ${data}`);
 
   superagent
