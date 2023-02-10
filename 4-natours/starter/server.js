c112354368d9ecb764b74413ad596a0717049b42
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-10 13:19:19 +0200

    Lesson 83

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+const pwd = process.env.DATABASE_PASSWORD;
+const db = process.env.DATABASE;
+const DB = db.replace('<PASSWORD>', pwd);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful !!'));
 
 const port = process.env.PORT || 3000;
 
