6b5c811c98c0c556801a7e24131fa4d6ac7f27a1
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-11 12:45:47 +0200

    Lesson 94

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const pwd = process.env.DATABASE_PASSWORD;
+const db = process.env.DATABASE;
+const DB = db.replace('<PASSWORD>', pwd);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful !!'));
+
+// READ JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA TO DB
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully delete');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
