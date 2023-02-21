c73ff26670d20f0d5374168f4715853eddec1256
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-21 13:04:14 +0200

    Lesson 127

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -19,12 +20,31 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    // minlength: 8,
+    minlength: 8,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //This way only works CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
