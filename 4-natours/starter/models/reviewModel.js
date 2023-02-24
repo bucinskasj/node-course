0edb0cac833e172f686e56b0281695329288071f
Jonas Bucinskas <jonas@bucinskas.io>
2023-02-24 23:29:43 +0200

    Lesson 154

---
+++ 4-natours/starter/models/reviewModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      require: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      require: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//review//rating
+//createdAt//reference to tour
+//ref to user who wrote a review
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
